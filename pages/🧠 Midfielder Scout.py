5b2c792f5623aa54906313526a8cd3e1cb8e805f
Mozi333 <52952159+Mozi333@users.noreply.github.com>
2022-11-04 19:22:56 +0100

    Update 🧠 Midfielder Scout.py

--- pages/🧠 Midfielder Scout.py
+++ pages/🧠 Midfielder Scout.py
@@ -23,6 +23,8 @@ import openpyxl
 from pathlib import Path
 from mplsoccer import PyPizza, add_image, FontManager
 import time
+#import new metrics module from new_metrics.py folder
+from new_metrics import *
 
 
 #-------- hide hamburger menu and made with streamlit text
@@ -58,49 +60,8 @@ def load_data():
     
     return df
 
-def new_metrics(df):  
     
-    #create Goles x remate metric
-    df["Goal %"] = round(df['Non-penalty goals'] / df['Shots'], 2) 
-    
-    #goal ratio
-    df['Goal Ratio'] = round(df['Shots'] / df['Non-penalty goals'], 2)
-    
-    #Shots minus penalties
-    
-    df['non_penalty_shots'] = df['Shots'] - df['Penalties taken']
-    
-    #Create new column 90 min played
-    df['90s'] = df['Minutes played'] / 90
-    df['90s'] = df['90s'].round()
-    
-    #Create column with penalty xG
-    df["penalty_xG"] = df['Penalties taken'] * 0.76 
-    
-    #Create column with  npxG
-    df["nonpenalty_xG"] = round(df['xG'] - df["penalty_xG"], 2) 
-    
-    #Create column with pxG per 90
-    df["penalty_xG/90"] = round(df['penalty_xG'] / df["90s"], 2) 
-    
-    #Create column with  npxG per 90
-    df["nonpenalty_xG/90"] = round(df['xG per 90'] - df["penalty_xG/90"], 2) 
-    
-    #Create column with  xG and npG per 90
-    df["Sum_xGp90_and_Goalsx90"] = round(df['nonpenalty_xG/90'] + df["Non-penalty goals per 90"], 2)
-
-    #Create column with  xA and Assist per 90
-    df["Sum_xAx90_and_Assistx90"] = round(df['xA per 90'] + df["Assists per 90"], 2) 
-    
-    
-    #goal difference from xG p90
-    df["xG_Difference"] = round(df['Non-penalty goals per 90'] - df['nonpenalty_xG/90'], 2)
-    
-    #xG per shot average
-    df['np_xG_per_shot_average'] =  df['nonpenalty_xG'] / df['non_penalty_shots']
-
-    
-#Dividir Playeres por posicion 
+#Divide players per position 
 
 def get_position():
 
@@ -179,12 +140,12 @@ minutes = st.sidebar.slider('Minutes:', 0, 3000, 360)
 st.sidebar.write('Filter players by market value:')
 market_value = st.sidebar.slider('Market Value:', 0, 100000000, 7000000)
 
-#Height
-st.sidebar.write('Filter players by height:')
-height_value = st.sidebar.slider('Height:', 0, 202, 170)
+# #Height
+# st.sidebar.write('Filter players by height:')
+# height_value = st.sidebar.slider('Height:', 0, 202, 170)
 
 
-df = df.loc[(df['Minutes played'] > minutes) & (df['Age'] < age) & (df['Position'] != 'GK') & (df['Market value'] < market_value) & (df['Height'] > height_value)]
+df = df.loc[(df['Minutes played'] > minutes) & (df['Age'] < age) & (df['Position'] != 'GK') & (df['Market value'] < market_value)]
 df.Player.unique()
 
 #-------ASSIGNT VALUES FOR LATER USE------------------
@@ -260,7 +221,8 @@ general_midfield_filter = ['Player',
                    'Shots per 90',
                    'Smart passes per 90',
                    'np_xG_per_shot_average',
-                   'Goal Ratio']
+                   'Goal Ratio',
+                   'PAdj Sliding tackles']
 
 #save DF with new column
 
@@ -532,7 +494,8 @@ with attacking_mid:
                               'Successful dribbles, %',
                               'Received passes per 90', 
                               'Accurate through passes, %',
-                              'Goal Ratio']]).copy()
+                              'Goal Ratio',
+                              'PAdj Sliding tackles']]).copy()
 
     #Sort By
 
@@ -734,6 +697,378 @@ with num6_mid:
                                                        'Defensive duels won, %',
                                                        'Accurate long passes, %',
                                                        'Aerial duels won, %']).set_precision(2))
+    
+
+
+    #-------------------------------------------------NUMBER 6 RADAR----------------------------------------
+
+    st.title('PERCENTILE RANKING RADAR')
+
+
+    #Define colors
+
+    Background = '#020807'
+    Defense = '#E67E22'
+    Passes = '#3f88c5'
+    Attack = '#E1005C'
+    Black = '#fffcf2' #Inner text color 
+    TempColor = '#FFD15C'
+    LeagueColor = '#FF3333'
+    PosColor = '#72A8D5'
+
+
+    #Title 
+    title_font = FontManager(("https://github.com/Mozi333/fonts/blob/main/RecoletaAlt-Black.ttf?raw=true"))
+
+    #Sutitle
+    subtitle_font = FontManager(("https://github.com/Mozi333/fonts/blob/main/RightGrotesk-Medium.otf?raw=true"))
+
+    #Values Text
+    font_text = FontManager(("https://github.com/google/fonts/blob/main/ofl/lato/Lato-SemiBold.ttf?raw=true"))
+    bio_text = FontManager(("https://github.com/google/fonts/blob/main/ofl/lato/Lato-SemiBold.ttf?raw=true"))
+
+
+    #------ Player name filter
+
+    #name has a list with all the player names
+    option = st.selectbox('Choose Player', (name))
+
+    st.write('You chose:', option)
+
+    fullname = st.text_input('Type name to print on radar', option)
+    st.write('The full name of the player is:', fullname)
+
+
+    #---------------- PRINT RADAR -----------------------------------
+    def radar(general_midfield_values, name, minutes, age, SizePlayer):
+
+
+        #-----Define Bio values
+
+        #Define Team
+
+        Team = general_midfield_values[general_midfield_values['Player']==option]
+        Team = Team['Team within selected timeframe'].item()
+
+        #Define Age
+
+        Age = general_midfield_values[general_midfield_values['Player']==option]
+        Age = Age['Age'].item()
+
+        #Define Height
+
+        Height = general_midfield_values[general_midfield_values['Player']==option]
+        Height = Height['Height'].item()
+
+        #Define Foot
+
+        Foot = general_midfield_values[general_midfield_values['Player']==option]
+        Foot = Foot['Foot'].item()
+
+
+        #Define Matches Played
+
+        Matches = general_midfield_values[general_midfield_values['Player']==option]
+        Matches = Matches['Matches played'].item()
+
+        #Define Goals
+
+        Goals = general_midfield_values[general_midfield_values['Player']==option]
+        Goals = Goals['Non-penalty goals'].item()
+
+        #Define Assists
+
+        Assists = general_midfield_values[general_midfield_values['Player']==option]
+        Assists = Assists['Assists'].item()
+
+        #Define Minutes Played
+
+        Minutesplayed = general_midfield_values[general_midfield_values['Player']==option]
+        Minutesplayed = Minutesplayed['Minutes played'].item()
+
+
+        #Define Market Value
+
+        Marketvalue = general_midfield_values[general_midfield_values['Player']==option]
+        Marketvalue = Marketvalue['Market value'].item()
+
+        #Define Contract Info
+
+        Contractexpires = general_midfield_values[general_midfield_values['Player']==option]
+        Contractexpires = Contractexpires['Contract expires'].item()
+
+        #Rename Values
+
+
+        general_midfield_values.rename(columns={
+            'xA per 90':'xA p90m',
+            'Successful defensive actions per 90':'Successful \ndefensive \nactions \np90m',
+            'Successful attacking actions per 90':'Successful \nattacking \nactions \np90m',
+            'Key passes per 90':'Key \npasses \np90m',
+            'Shots on target, %':'% Shots \non target',
+            'Offensive duels won, %':'% Offensive \nduels won',
+            'Goal Ratio':'Goal \nRatio',
+            'nonpenalty_xG/90':'xG \np90m',
+            'Accurate passes to final third, %':'% Accurate \npasses to \nfinal third',
+            'Non-penalty goals per 90':'Goals \np90m',
+            'Shots per 90':'Shots \np90m',
+            'Accurate through passes, %':'% Accurate \nthrough \npasses',
+            'PAdj Interceptions':'PAdj \nInterceptions',
+            'Received passes per 90':'Received \npasses \np90m',
+            'Defensive duels won, %':'% Defensive \nduels \nwon',
+            'np_xG_per_shot_average':'xG per \nshot \navg',
+            'Aerial duels won, %':'% Aerial \nduels \nwon',
+            'PAdj Sliding tackles':'PAdj \nSliding \ntackles'}, inplace=True)
+
+        #REORDER COLUMNS
+
+        #This only effects the final order of the table
+
+        general_midfield_values = general_midfield_values[[
+                'Player',
+                'PAdj \nInterceptions',
+                'PAdj \nSliding \ntackles',
+                '% Defensive \nduels \nwon',
+                '% Aerial \nduels \nwon',
+                'xG \np90m',
+                'Goals \np90m',
+                'Goal \nRatio',
+                'Shots \np90m',
+                'xG per \nshot \navg',
+                'Successful \nattacking \nactions \np90m',
+                'xA p90m',
+                'Key \npasses \np90m',
+                '% Accurate \npasses to \nfinal third',
+                '% Accurate \nthrough \npasses',
+                'Received \npasses \np90m']]
+
+        #Create a parameter list
+
+        params = list(general_midfield_values.columns)
+
+        #drop player column
+
+        params = params[1:]
+
+        # Now we filter the df for the player we want.
+        # The player needs to be spelled exactly the same way as it is in the data. Accents and everything 
+
+        player = general_midfield_values.loc[general_midfield_values['Player']==option].reset_index()
+        player = list(player.loc[0]) #gets all values/rows of specific player
+        player = player[2:]
+
+        # now that we have the player scores, we need to calculate the percentile values with scipy stats.
+        # I am doing this because I do not know the percentile beforehand and only have the raw numbers
+
+        values = []
+        for x in range(len(params)):   
+            values.append(math.floor(stats.percentileofscore(general_midfield_values[params[x]],player[x])))
+
+
+        #------Plot Radar
+
+        # color for the slices and text
+        slice_colors = [Defense] * 4 + [Attack] * 6 + [Passes] * 5  # ataque - pases 
+        text_colors = ["#F2F2F2"] * 15
+
+        # instantiate PyPizza class
+        baker = PyPizza(
+            params=params,                  # list of parameters
+            background_color=Background,     # background color
+            straight_line_color="#FFFFFF",  # color for straight lines
+            straight_line_lw=1,             # linewidth for straight lines
+            last_circle_lw=0,               # linewidth of last circle
+            other_circle_lw=0,              # linewidth for other circles
+            inner_circle_size=9            # size of inner circle
+        )
+
+        # plot pizza
+        fig, ax = baker.make_pizza(
+            values,                          # list of values
+            figsize=(6,4),                   # adjust figsize according to your need
+            param_location=115,              # where the parameter names will be added adjust position
+            color_blank_space="same",        # use same color to fill blank space
+            slice_colors=slice_colors,       # color for individual slices
+            value_colors=text_colors,        # color for the value-text
+            value_bck_colors=slice_colors,   # color for the blank spaces
+            blank_alpha=0.4,                 # alpha for blank-space colors
+            kwargs_slices=dict(
+                edgecolor="#FFFFFF", zorder=2, linewidth=2
+            ),                               # values to be used when plotting slices
+            kwargs_params=dict(
+                color="#FFFFFF", fontsize=7, #size of value titles
+                fontproperties=font_text.prop, va="center"
+            ),                               # values to be used when adding parameter
+            kwargs_values=dict(
+                color="#000000", fontsize=7, #size of values numbers
+                fontproperties=font_text.prop, zorder=3,
+                bbox=dict(
+                    edgecolor="#FFFFFF", facecolor="cornflowerblue",
+                    boxstyle="round,pad=0.2", lw=1
+                )
+            )                                # values to be used when adding parameter-values
+        )
+
+
+        # add name title
+
+        fig_text(0, 1.18, f'<{str(fullname.upper(),)}>', size = SizePlayer,  fontproperties=title_font.prop, color=Black, 
+            highlight_textprops=[{'color':Black}])
+
+
+        # add credits
+        notes = '* Only players with more than ' + str(minutes) + ' minutes played' + '\n* Only players under ' + str(age) + '\n* Penalty goals are not counted' + '\n Data from: Wyscout'
+
+
+        fig.text(
+            1.1, -0.09, f"{notes}", size=7, #does not include player text
+            fontproperties=font_text.prop, color=Black,
+            ha="right"
+        )
+
+        # add rectangle text
+
+        fig.text(
+            0.78, .051, "Passing", size=6,     #passing
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        # add text
+        fig.text(
+            0.86, 0.051, "Defense", size=6,   #defense
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        # add text
+        fig.text(
+            0.95, 0.051, "Attack", size=6,    #attack
+            fontproperties=bio_text.prop, color=Black
+        )
+
+
+
+
+        # add rectangles
+        fig.patches.extend([
+
+            plt.Rectangle(
+                (0.75, 0.05), 0.025, 0.021, fill=True, color=Passes, #passing
+                transform=fig.transFigure, figure=fig
+            ),
+
+            plt.Rectangle(
+                (0.83, 0.05), 0.025, 0.021, fill=True, color=Defense, #defense
+                transform=fig.transFigure, figure=fig
+            ),
+
+            plt.Rectangle(
+                (0.92, 0.05), 0.025, 0.021, fill=True, color=Attack, #attack
+                transform=fig.transFigure, figure=fig
+            ),
+
+
+        ])
+
+        #BIO----------
+
+
+        #team
+
+        fig.text(
+
+
+            0.90, 0.40,  str(Team), size=10,    #team
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Age
+
+        fig.text(
+
+
+            0.90, 0.35,  "Age: " + str(Age), size=7,    #Edad
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Altura
+
+        fig.text(
+
+
+            0.90, 0.32,  "Height: " + str(Height), size=7,    #Height
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Foot
+
+        fig.text(
+
+
+            0.90, 0.29,  "Foot: " + str(Foot), size=7,    #Foot
+            fontproperties=bio_text.prop, color=Black
+        )
+
+
+
+        #Matches Played
+
+        fig.text(
+
+
+            0.90, 0.26,  "Matches: " + str(Matches), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Minutes played
+
+        fig.text(
+
+
+            0.90, 0.23,  "Minutes played: " + str(Minutesplayed), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Goals
+
+        fig.text(
+
+
+            0.90, 0.20,  "Goals: " + str(Goals), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Assists
+
+        fig.text(
+
+
+            0.90, 0.17,  "Assists: " + str(Assists), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+
+        #Market value
+
+        fig.text(
+
+
+            0.90, 0.14,  "Market value:  €" + str(f"{Marketvalue:,}"), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        #Contract info
+
+        fig.text(
+
+
+            0.90, 0.11,  "Contract exp: " + str(Contractexpires), size=7,    
+            fontproperties=bio_text.prop, color=Black
+        )
+
+        st.pyplot(fig)
+
+
+    radar(general_midfield_values, option, minutes, age, SizePlayer = 45)
 
 #--------------------------------------- TABS ------------------------------
 
@@ -993,7 +1328,7 @@ bio_text = FontManager(("https://github.com/google/fonts/blob/main/ofl/lato/Lato
 #------ Player name filter
 
 #name has a list with all the player names
-option = st.selectbox('Choose Player', (name))
+option = st.selectbox('Choose Player name', (name))
 
 st.write('You chose:', option)
 
